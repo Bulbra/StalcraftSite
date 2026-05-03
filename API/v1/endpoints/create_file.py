b481a9f417eaf336ce48b4610c43aea52269bf8e
Bulbra <darthartem76@gmail.com>
2026-05-03 11:33:28 +0300

    fix endpoint

--- API/v1/endpoints/create_file.py
+++ API/v1/endpoints/create_file.py
@@ -7,9 +7,9 @@ router = APIRouter(
 
 @router.get("/user_file")
 async def generate_password(filename, text):
-    if not os.Path.Exists(filename):
+    if not os.path.exists(filename):
         with open(filename, 'w', encoding='utf-8') as file:
-            file.Write(f'{text}\n')
+            file.write(f'{text}\n')
             print(f'Файл {filename} создан')
     else:
         print(f'Файл {filename} уже существует')
